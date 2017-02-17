ac50fd731df13688450e1e868cc351f6bd6d274c
Lester Fichtner <lfichtne@us.ibm.com>
2017-02-17 14:48:02 -0600

    scripts currently expect /backups not /backup

--- lvm_setup.sh
+++ lvm_setup.sh
@@ -5,4 +5,4 @@ vgcreate backup-dump /dev/xvdb
 lvcreate -n backup-dump-vol --extents 100%FREE backup-dump
 mkfs.xfs /dev/backup-dump/backup-dump-vol 
 blkid | grep backup--dump
-echo "Add the above UUID to /etc/fstab mapping it to /backup"
+echo "Add the above UUID to /etc/fstab mapping it to /backups"
